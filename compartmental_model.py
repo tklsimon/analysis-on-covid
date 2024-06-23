3238b161ea179166c043cc7aa60b2909af4f0216
tklsimon <126179157+tklsimon@users.noreply.github.com>
2024-06-23 11:02:21 +0800

    Enhancement on Compartmental Model
    - Fix bug for get_date_count, previously only got 2020 counts
    - Update 1st wave range
    - Add self.classes for SIR, SIS, SIRS, SEIR
    - Correct plt.legend for plot_fitted_result
    - Simplified init_params setting using default values instead of None

--- compartmental_model.py
+++ compartmental_model.py
@@ -9,13 +9,15 @@ from sklearn import metrics
 def composite_function(f, g): 
     return lambda *x : f(g(*x))
 
-def plot_fitted_result(predict_count_lists, true_I_counts):
+def plot_fitted_result(predict_count_lists: List[List[int]],
+                       true_I_counts: List[int],
+                       classes: List[str]):
     fig = plt.figure(figsize=(12, 4))
     for counts in predict_count_lists:
         plt.plot(counts)
-    plt.plot(np.arange(0, len(true_I_counts)), true_I_counts, "k*:")
-    plt.grid("True")
-    plt.legend(["Susceptible","Infected","Removed","Original Data"])
+    plt.plot(np.arange(0, len(true_I_counts)), true_I_counts, 'k*:')
+    plt.grid('True')
+    plt.legend(classes + ['Original Data'])
     plt.plot()
 
 
@@ -24,7 +26,7 @@ class Compartmental_model:
     def __init__(self,
                  I_counts: List[int],
                  initial_counts: List[int],
-                 I_pos: int = 1):
+                 I_pos: int=1):
         self.I_counts = I_counts
         self.initial_counts = initial_counts
         self.I_pos = I_pos
@@ -40,7 +42,7 @@ class Compartmental_model:
     def calculate_cost(self, predicted_I: List[int]):
         return self.cost_function(predicted_I, self.I_counts)
     
-    def solve(self, params: List[float], initial_counts: List[int], t_length: int, pos: int = None):
+    def solve(self, params: List[float], initial_counts: List[int], t_length: int, pos: int=None):
         sol = scipy.integrate.odeint(self.DE, initial_counts, range(0, t_length), args=tuple(params)).T
         if pos:
             return sol[pos]
@@ -60,15 +62,13 @@ class Compartmental_model:
 
 # SIR Model (Child Class)
 class SIR_model(Compartmental_model):
-    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params=None):
+    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params: List[float]=(0.001, 1)):
         super().__init__(I_counts, initial_counts)
-        if init_params:
-            self.init_params = init_params
-        else:
-            self.init_params = (0.001, 1)
+        self.classes = ['Susceptible', 'Infectious', 'Recovered']
+        self.init_params = init_params
     
     @staticmethod
-    def DE(initial_counts: List[int], t, *params):
+    def DE(initial_counts: List[int], t, *params: List[float]):
         if len(initial_counts) != 3:
             raise Exception('Length of initial_counts should be 3.')
         if len(params) != 2:
@@ -84,15 +84,13 @@ class SIR_model(Compartmental_model):
 
 # SIS Model (Child Class)
 class SIS_model(Compartmental_model):
-    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params=None):
+    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params: List[float]=(0.001, 1)):
         super().__init__(I_counts, initial_counts)
-        if init_params:
-            self.init_params = init_params
-        else:
-            self.init_params = (0.001, 1)
+        self.classes = ['Susceptible', 'Infectious']
+        self.init_params = init_params
     
     @staticmethod
-    def DE(initial_counts, t, *params):
+    def DE(initial_counts: List[int], t, *params: List[float]):
         if len(initial_counts) != 2:
             raise Exception('Length of initial_counts should be 2.')
         if len(params) != 2:
@@ -107,15 +105,13 @@ class SIS_model(Compartmental_model):
 
 # SIRS Model (Child Class)
 class SIRS_model(Compartmental_model):
-    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params=None):
+    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params=(0.001 , 1, 0.1)):
         super().__init__(I_counts, initial_counts)
-        if init_params:
-            self.init_params = init_params
-        else:
-            self.init_params = (0.001, 1)
+        self.classes = ['Susceptible', 'Infectious', 'Recovered']
+        self.init_params = init_params
     
     @staticmethod
-    def DE(initial_counts, t, *params):
+    def DE(initial_counts: List[int], t, *params: List[float]):
         if len(initial_counts) != 3:
             raise Exception('Length of initial_counts should be 3.')
         if len(params) != 3:
@@ -131,15 +127,13 @@ class SIRS_model(Compartmental_model):
 
 # SEIR Model (Child Class)
 class SEIR_model(Compartmental_model):
-    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params=None):
+    def __init__(self, I_counts: List[int], initial_counts: List[int], init_params: List[float]=(0.001 , 1, 0.1)):
         super().__init__(I_counts, initial_counts, 2)
-        if init_params:
-            self.init_params = init_params
-        else:
-            self.init_params = (0.001, 1)
+        self.classes = ['Susceptible', 'Exposed', 'Infectious', 'Recovered']
+        self.init_params = init_params
     
     @staticmethod
-    def DE(initial_counts, t, *params):
+    def DE(initial_counts: List[int], t, *params: List[float]):
         if len(initial_counts) != 4:
             raise Exception('Length of initial_counts should be 4.')
         if len(params) != 3:
