24b0dc152fb6236ffda65a13cc2d5aabb20042cf
tklsimon <126179157+tklsimon@users.noreply.github.com>
2024-10-16 20:08:34 +0800

    Updates on Discrete Compartmental Module
    For plot result function,
    - Use the fitted model instead of count list and class names
    - Added date index to display dates in x-axis
    - Update x label
    
    For Parent Class,
    - Add set_true_plus_list to set self.true_plus_list
    - Move the input for __init__ to child classes
    - Update the fit function that input counts are required now
    
    For Child Class,
    - Add N
    - Update the lower bounds for parameters
    - Update the formulae for beta
    - Update the init_params values

--- discrete_compartmental_model.py
+++ discrete_compartmental_model.py
@@ -6,36 +6,31 @@ import matplotlib.pyplot as plt
 def composite_function(f, g): 
     return lambda *x : f(g(*x))
 
-def plot_compartmental_model_result(model_output,
-                                    true_plus_list,
-                                    classes):
+def plot_compartmental_model_result(index_x_dt,
+                                    fitted_model,
+                                    true_plus_list):
     line_color_dict = {'Susceptible': 'b',
                        'Exposed': 'm',
                        'Infectious': 'r',
                        'Recovered': 'g'}
     fig = plt.figure(figsize=(12, 4))
-    predicted_plus_list, compartment_counts_list = model_output
-    for counts, class_name in zip(compartment_counts_list, classes):
-        plt.plot(counts, line_color_dict[class_name])
-    plt.plot(predicted_plus_list[classes.index('Infectious')-1], 'r--')
-    plt.plot(true_plus_list[0], 'k*:')
+    predicted_plus_list, compartment_counts_list = fitted_model.output
+    for counts, class_name in zip(compartment_counts_list, fitted_model.classes):
+        plt.plot(index_x_dt, counts, line_color_dict[class_name])
+    plt.plot(index_x_dt, predicted_plus_list[fitted_model.classes.index('Infectious')-1], 'r--')
+    plt.plot(index_x_dt, true_plus_list[0], 'k*:')
     plt.title('Compartmental Model Result')
-    plt.xlabel('Days')
+    plt.xlabel('Date')
     plt.ylabel('Count')
     plt.grid('True')
-    plt.legend(classes + ['Predicted new case counts'] + ['True new case counts'])
+    plt.legend(fitted_model.classes + ['Predicted new case counts'] + ['True new case counts'])
     plt.plot()
 
 # Discrete Compartmental Model (Base Class)
 class Discrete_Compartmental_model:
     def __init__(self,
-                 true_plus_list,
-                 initial_counts,
                  init_params):
-        self.true_plus_list = true_plus_list
-        self.initial_counts = initial_counts
         self.init_params = init_params
-        self.true_plus_list_nbr, self.t_length = self.true_plus_list.shape
     
     @staticmethod
     def cost_function(predicted, true):
@@ -43,6 +38,10 @@ class Discrete_Compartmental_model:
         for _ in range(len(cost.shape)):
             cost = sum(cost)
         return cost
+    
+    def set_true_plus_list(self,
+                           true_plus_list):
+        self.true_plus_list = true_plus_list
     
     def calculate_cost(self, predicted_DI_plus_list):
         return self.cost_function(predicted_DI_plus_list, self.true_plus_list)
@@ -53,30 +52,39 @@ class Discrete_Compartmental_model:
               output=None):
         pass
     
-    def get_optm_params(self):
-        if self.true_plus_list_nbr == 1:
+    def get_optm_params(self,
+                        true_plus_list,
+                        initial_counts):
+        if true_plus_list.shape[0] == 1:
             model_output = 'DI_plus_list'
-        elif self.true_plus_list_nbr > 1:
+        elif true_plus_list.shape[0] > 1:
             model_output = 'plus_list'
+            
+        self.set_true_plus_list(true_plus_list)
+        
         sol = scipy.optimize.minimize(
             composite_function(self.calculate_cost, self.solve),
             self.init_params,
-            (self.initial_counts, self.t_length, model_output),
+            (initial_counts, true_plus_list.shape[1], model_output),
             method='Nelder-Mead')
+        
         return sol.x
     
-    def fit(self):
-        optm_params = self.get_optm_params()
-        print('The optimal parameters are:', optm_params)
-        return self.solve(optm_params, self.initial_counts, self.t_length)
+    def fit(self,
+            true_plus_list,
+            initial_counts):
+        print('Fitting compartmental model ...')
+        optm_params = self.get_optm_params(true_plus_list, initial_counts)
+        print('The optimal parameters found are:', optm_params)
+        print('Generating the counts as model output ...')
+        self.output = self.solve(optm_params, initial_counts, true_plus_list.shape[1])
+        print('Completed.')
 
 # Discrete SIR Model (Child Class)
 class Discrete_SIR_model(Discrete_Compartmental_model):
     def __init__(self,
-                 true_plus_list,
-                 initial_counts,
-                 init_params=(0.001, 0.1)):
-        super().__init__(true_plus_list, initial_counts, init_params)
+                 init_params=(8e-1, 1e-1)):
+        super().__init__(init_params)
         self.classes = ['Susceptible', 'Infectious', 'Recovered']
 
     @staticmethod
@@ -85,15 +93,16 @@ class Discrete_SIR_model(Discrete_Compartmental_model):
               t_length,
               output=None):
         S_0, I_0, R_0 = initial_counts
+        N = sum(initial_counts)
         beta, gamma = params
     
-        beta = min(max(beta, 0), 1)
-        gamma = min(max(gamma, 0), 1)
+        beta = min(max(beta, 1e-5), 10)
+        gamma = min(max(gamma, 1e-5), 1)
     
         S_list, I_list, R_list = [S_0], [I_0], [R_0]
         DI_plus_list, DR_plus_list = [I_0], [R_0]
         for _ in range(t_length-1):
-            DI_plus = int(beta*S_list[-1]*I_list[-1])
+            DI_plus = int(beta*S_list[-1]*I_list[-1]/N)
             DR_plus = int(gamma*I_list[-1])
     
             if DI_plus > S_list[-1]:
@@ -121,10 +130,8 @@ class Discrete_SIR_model(Discrete_Compartmental_model):
 # Discrete SIS Model (Child Class)
 class Discrete_SIS_model(Discrete_Compartmental_model):
     def __init__(self,
-                 true_plus_list,
-                 initial_counts,
-                 init_params=(0.001, 0.00001)):
-        super().__init__(true_plus_list, initial_counts, init_params)
+                 init_params=(8e-1, 5e-2)):
+        super().__init__(init_params)
         self.classes = ['Susceptible', 'Infectious']
 
     @staticmethod
@@ -133,15 +140,16 @@ class Discrete_SIS_model(Discrete_Compartmental_model):
               t_length,
               output=None):
         S_0, I_0 = initial_counts
+        N = sum(initial_counts)
         beta, gamma = params
     
-        beta = min(max(beta, 0), 1)
-        gamma = min(max(gamma, 0), 1)
+        beta = min(max(beta, 1e-5), 10)
+        gamma = min(max(gamma, 1e-5), 1)
     
         S_list, I_list = [S_0], [I_0]
         DI_plus_list, DS_plus_list = [I_0], [0]
         for _ in range(t_length-1):
-            DI_plus = int(beta*S_list[-1]*I_list[-1])
+            DI_plus = int(beta*S_list[-1]*I_list[-1]/N)
             DS_plus = int(gamma*I_list[-1])
     
             if DI_plus > S_list[-1]: # Not S_list[-1] + DS_plus, to avoid circular update issue
@@ -169,10 +177,8 @@ class Discrete_SIS_model(Discrete_Compartmental_model):
 # Discrete SIRS Model (Child Class)
 class Discrete_SIRS_model(Discrete_Compartmental_model):
     def __init__(self,
-                 true_plus_list,
-                 initial_counts,
-                 init_params=(0.001, 0.2, 0.1)):
-        super().__init__(true_plus_list, initial_counts, init_params)
+                 init_params=(8e-1, 1e-1, 1e-2)):
+        super().__init__(init_params)
         self.classes = ['Susceptible', 'Infectious', 'Recovered']
 
     @staticmethod
@@ -181,16 +187,17 @@ class Discrete_SIRS_model(Discrete_Compartmental_model):
               t_length,
               output=None):
         S_0, I_0, R_0 = initial_counts
+        N = sum(initial_counts)
         beta, gamma, sigma = params
     
-        beta = min(max(beta, 0), 1)
-        gamma = min(max(gamma, 0), 1)
-        sigma = min(max(sigma, 0), 1)
+        beta = min(max(beta, 1e-5), 10)
+        gamma = min(max(gamma, 1e-5), 1)
+        sigma = min(max(sigma, 1e-5), 1)
     
         S_list, I_list, R_list = [S_0], [I_0], [R_0]
         DI_plus_list, DR_plus_list, DS_plus_list = [I_0], [R_0], [0]
         for _ in range(t_length-1):
-            DI_plus = int(beta*S_list[-1]*I_list[-1])
+            DI_plus = int(beta*S_list[-1]*I_list[-1]/N)
             DR_plus = int(gamma*I_list[-1])
             DS_plus = int(sigma*R_list[-1])
     
@@ -223,10 +230,8 @@ class Discrete_SIRS_model(Discrete_Compartmental_model):
 # Discrete SEIR Model (Child Class)
 class Discrete_SEIR_model(Discrete_Compartmental_model):
     def __init__(self,
-                 true_plus_list,
-                 initial_counts,
-                 init_params=(0.0005, 0.8, 0.1)):
-        super().__init__(true_plus_list, initial_counts, init_params)
+                 init_params=(8e-1, 5e-1, 1e-1)):
+        super().__init__(init_params)
         self.classes = ['Susceptible', 'Exposed', 'Infectious', 'Recovered']
 
     @staticmethod
@@ -235,16 +240,17 @@ class Discrete_SEIR_model(Discrete_Compartmental_model):
               t_length,
               output=None):
         S_0, E_0, I_0, R_0 = initial_counts
+        N = sum(initial_counts)
         beta, alpha, gamma = params
     
-        beta = min(max(beta, 0.000001), 1)
-        alpha = min(max(alpha, 0.001), 1)
-        gamma = min(max(gamma, 0.001), 1)
+        beta = min(max(beta, 1e-5), 10)
+        alpha = min(max(alpha, 1e-5), 1)
+        gamma = min(max(gamma, 1e-5), 1)
     
         S_list, E_list, I_list, R_list = [S_0], [E_0], [I_0], [R_0]
         DE_plus_list, DI_plus_list, DR_plus_list = [E_0], [I_0], [R_0]
         for _ in range(t_length-1):
-            DE_plus = int(beta*S_list[-1]*I_list[-1])
+            DE_plus = int(beta*S_list[-1]*I_list[-1]/N)
             DI_plus = int(alpha*E_list[-1])
             DR_plus = int(gamma*I_list[-1])
     
